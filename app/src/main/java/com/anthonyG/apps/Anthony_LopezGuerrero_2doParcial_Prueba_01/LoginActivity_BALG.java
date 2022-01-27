3a56c4cf0ef130ccd966b3273e6154fb276f0bd9
Anthony <anthonyxm15@gmail.com>
2022-01-26 19:30:41 -0500

    19h30

--- app/src/main/java/com/anthonyG/apps/Anthony_LopezGuerrero_2doParcial_Prueba_01/LoginActivity_BALG.java
+++ app/src/main/java/com/anthonyG/apps/Anthony_LopezGuerrero_2doParcial_Prueba_01/LoginActivity_BALG.java
@@ -25,8 +25,6 @@ public class LoginActivity_BALG extends AppCompatActivity {
         editTextPasswd = findViewById(R.id.editTextPassword1);
 
         buttonLogin = findViewById(R.id.buttonLogin);
-        buttonCrud = findViewById(R.id.buttonCrud);
-
 
         buttonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,31 +32,19 @@ public class LoginActivity_BALG extends AppCompatActivity {
                 ValidarUsuario(view);
             }
         });
-
-        buttonCrud.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                EditUserInformation(view);
-            }
-        });
     }
 
     private void ValidarUsuario(View view) {
 
-        if(editTextUsr.getText().equals("anthony") || editTextUsr.getText().equals("alexis") && editTextPasswd.getText().equals("anthony123") || editTextPasswd.getText().equals("anthony123"))
+        if (editTextUsr.getText().toString().equals("anthony") && editTextPasswd.getText().toString().equals("anthony123")
+                || (editTextUsr.getText().toString().equals("alexis") && editTextPasswd.getText().toString().equals("alexis123")))
         {
             Intent flags = new Intent(this, MainActivity_BALG.class);
             startActivity(flags);
         }
         else
         {
-            Toast.makeText(this, "Usuario o contraseña incorrectos", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Usuario o contraseña incorrecto", Toast.LENGTH_SHORT).show();
         }
-
-    }
-
-    private void EditUserInformation(View view) {
-        Intent crudActivity = new Intent(this, CrudClientes_BALG.class);
-        startActivity(crudActivity);
     }
 }
